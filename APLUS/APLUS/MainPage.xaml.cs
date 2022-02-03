7330535323af225dfa229d61fbb21e26e382229a
Владислав Хлыбов <vavan8765@gmail.com>
2022-02-03 15:59:08 +0300

    release 1.0

--- APLUS/APLUS/MainPage.xaml.cs
+++ APLUS/APLUS/MainPage.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using static APLUS.MainPage;
 
+[assembly: Dependency(typeof(SQLite_Android))]
 namespace APLUS
 {
     public partial class MainPage : ContentPage
@@ -14,6 +17,21 @@ namespace APLUS
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        public interface ISQLite
+        {
+            string GetDatabasePath(string filename);
+        }
+        public class SQLite_Android : ISQLite
+        {
+            public SQLite_Android() { }
+            public string GetDatabasePath(string sqliteFilename)
+            {
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                var path = Path.Combine(documentsPath, sqliteFilename);
+                return path;
+            }
         }
 
         private void Button_Clicked(object sender, EventArgs e)
